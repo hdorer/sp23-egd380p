6b87ec1b4ff4641af0cb584275ca576854b08d1f
Nathan Desjardins <nDesjardins@mtl.local>
2023-01-24 12:16:00 -0500

    adding player movement

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10;
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+}
