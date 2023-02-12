b0a0e317fbad7f46ba53cfb1a2002d51610df4b2
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-02-11 21:05:32 -0500

    Basic message parsing

--- Assets/Scripts/TwitchListener.cs
+++ Assets/Scripts/TwitchListener.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 
 public class TwitchListener : MonoBehaviour {
     [SerializeField] IRC twitchIrc;
+
+    private int[] votes = new int[4];
+    private string[] voteStrings = new string[4] { "1", "2", "3", "4" };
     
     private void OnEnable() {
-        twitchIrc.OnChatMessage += showChatMessage;
+        twitchIrc.OnChatMessage += parseChatMessage;
     }
 
-    private void showChatMessage(Chatter chatter) {
-        Debug.Log(chatter.message);
+    private void OnDisable() {
+        twitchIrc.OnChatMessage -= parseChatMessage;
+    }
+
+    private void parseChatMessage(Chatter chatter) {
+        for(int i = 0; i < voteStrings.Length; i++) {
+            if(chatter.message == voteStrings[i]) {
+                votes[i]++;
+            }
+        }
+
+        string logOutput = "";
+        for(int i = 0; i < voteStrings.Length; i++) {
+            logOutput += voteStrings[i] + ": " + votes[i] + ", ";
+        }
+
+        Debug.Log(logOutput);
     }
 }
