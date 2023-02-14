ad87ac94ee3bef5036fe7956b9d0ab9701abae09
nicolas.delbue <nicolas.delbue@mymail.champlain.edu>
2023-02-14 15:04:37 -0500

    Movement script final
    Movement working based on Y rotation, Character rotates based on where your mouse is on screen.

--- Assets/Scripts/NickScripts/MovementScript.cs
+++ Assets/Scripts/NickScripts/MovementScript.cs
@@ -6,22 +6,33 @@ public class MovementScript : MonoBehaviour
 {
     Rigidbody rb;
     public float runSpeed = 10.0f;
-
     float horiz;
     float vert;
-
     public Camera cam;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        GetInputs();
+        GetInputs();    //Simple get inputs
+
+        RotatePlayer(); //Rotates the player to the mouse position
+    }
+
+    void RotatePlayer()
+    {
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+ 
+        if(ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 
     void GetInputs() //Where you would put player input checks
@@ -30,7 +41,7 @@ public class MovementScript : MonoBehaviour
         vert = Input.GetAxisRaw("Vertical");
     }
 
-    void MovePlayer()
+    void MovePlayer()   //Moves player based on y rotation, so away from camera is always W and so on so forth.
     {
         Vector3 movement = new Vector3(horiz, 0, vert);
         movement = Quaternion.Euler(0,cam.transform.eulerAngles.y,0)*movement;
