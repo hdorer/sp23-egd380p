168c38e25a7398c11fcd402ffbaee6aca2b2dcee
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-02-21 12:15:30 -0500

    Added new spherical bullet prefab

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     [SerializeField] private float speed = 10;
-    [SerializeField] private float damage = 0;
+    private float damage = 0;
     private bool damageSet = false;
+
+    [SerializeField] private float lifetime = 15;
+
+    private void Start() {
+        Destroy(gameObject, lifetime);
+    }
 
     private void Update() {
         transform.position += transform.forward * Time.deltaTime * speed;
