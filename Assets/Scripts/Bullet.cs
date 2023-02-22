802771f76fecb022e70b8bb97a3e126a30c2cdf4
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-02-22 14:38:04 -0500

    That merge did not work correctly, so I fixed it

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
     
-
+public class Bullet : MonoBehaviour {
+    [SerializeField] private float speed = 10;
+    private float damage;
+    private bool damageSet = false;
+    
     private void Update() {
         transform.position += transform.forward * Time.deltaTime * speed;
     }
