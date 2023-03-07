f01cd41fd5a41282fcdda113564b10ca316a4082
Nathan Desjardins <nDesjardins@mtl.local>
2023-03-07 11:51:55 -0500

    adding more enemy types

--- Assets/Scripts/Enemy/States/Pursuit.cs
+++ Assets/Scripts/Enemy/States/Pursuit.cs
@@ -19,6 +19,11 @@ public class Pursuit : State
             enemy.GetComponent<Animator>().SetBool("IsWalking", true);
             enemy.agent.SetDestination(enemy.target.transform.position);
         }
+        if (enemy.gun != null)
+        {
+            Vector3 shootTarget = enemy.target.transform.position;
+            enemy.gun.transform.LookAt(shootTarget, enemy.transform.up);
+        }
         return this;
     }
 }
