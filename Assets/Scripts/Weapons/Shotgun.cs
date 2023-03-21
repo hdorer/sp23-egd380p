137e13de1d87aef53c6efca31b399ca99a0fb2b7
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-03-21 10:44:55 -0400

    Split Weapon into Weapon and Shotgun classes

---
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Shotgun")]
+public class Shotgun : Weapon {
+    [Header("Shotgun Properties")]
+    [SerializeField] private float bulletsPerShot;
+    [SerializeField] private float bulletSpread;
+
+    public override void fire(Transform bulletSpawnPoint) {
+        float startingRotation = -((bulletsPerShot - 1) / 2 * bulletSpread);
+        for(int i = 0; i < bulletsPerShot; i++) {
+            float bulletRotationX = bulletSpawnPoint.rotation.eulerAngles.x;
+            float bulletRotationY = bulletSpawnPoint.rotation.eulerAngles.y + startingRotation + bulletSpread * i;
+            float bulletRotationZ = bulletSpawnPoint.rotation.eulerAngles.z;
+
+            Quaternion bulletRotation = Quaternion.Euler(bulletRotationX, bulletRotationY, bulletRotationZ);
+
+            Bullet bullet = Instantiate(BulletPrefab, bulletSpawnPoint.position, bulletRotation).GetComponent<Bullet>();
+            bullet.setDamage(DamagePerBullet);
+        }
+    }
+}
