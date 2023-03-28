884de56fc7652e43aa875694febf79ff2a88920f
nicolas.delbue <nicolas.delbue@mymail.champlain.edu>
2023-03-28 15:22:14 -0400

    Fixed UI For Enemies

--- Assets/Scripts/NickScripts/EnemyHealthUI.cs
+++ Assets/Scripts/NickScripts/EnemyHealthUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class EnemyHealthUI : MonoBehaviour
 {
     public Enemy parentEnemy;
+    public Transform camPos;
     public Image curHP;
     private float floatMax, floatCur;
     // Start is called before the first frame update
@@ -13,11 +14,13 @@ public class EnemyHealthUI : MonoBehaviour
     {
         floatMax = (float)parentEnemy.MaxHealth/100;
         curHP.fillAmount = floatMax;
+        camPos = GameObject.Find("Main Camera").transform;
     }
     // Update is called once per frame
     void Update()
     {
         floatCur = (float)parentEnemy.Health/100;
         curHP.fillAmount = floatCur;
+        transform.LookAt(camPos.position - camPos.forward);
     }
 }
