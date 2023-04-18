0f34890dab1b1eada819364452f630442e000489
Samuel Fox <Samuel.Fox@mymail.champlain.edu>
2023-04-18 11:05:28 -0400

    working on fade out on level completion

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ExitLevel : MonoBehaviour
 {
     public int playerLayer;
     private LevelLoading loader;
+    private Image fadeOut;
 
     private void Awake()
     {
         loader = FindObjectOfType<LevelLoading>();
+        fadeOut = FindObjectOfType<FadeOut>().fadeImage;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == playerLayer)
-            loader.LoadNextLevel();
+            StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        // loop over 1 second backwards
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+            // set color with i as alpha
+            fadeOut.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        loader.LoadNextLevel();
     }
 }
