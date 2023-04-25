c6be7c53e0a67a2e8ea4efade51136ba72843f02
hcdorer <41219697+hcdorer@users.noreply.github.com>
2023-04-25 10:33:35 -0400

    Added other event

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -25,6 +25,20 @@ public class AnalyticsManager : MonoBehaviour {
         AnalyticsService.Instance.CustomData("pollStart", parameters);
         AnalyticsService.Instance.Flush();
 
-        Debug.Log("event sent");
+        Debug.Log("start event sent");
+    }
+
+    public void sendPollEndEvent(Poll poll) {
+        Dictionary<string, object> parameters = new Dictionary<string, object>() {
+            { "pollEffectVotes", poll.votes[0] },
+            { "pollEffectVotes1", poll.votes[1] },
+            { "pollEffectVotes2", poll.votes[2] },
+            { "pollEffectVotes3", poll.votes[3] }
+        };
+
+        AnalyticsService.Instance.CustomData("pollEnd", parameters);
+        AnalyticsService.Instance.Flush();
+
+        Debug.Log("end event sent");
     }
 }
